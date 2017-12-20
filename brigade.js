34120b23762ef81342450a1e9606d044068f2762
james.xiong <james.xiong@daocloud.io>
2017-12-20 22:29:51 +0800

    fix

--- brigade.js
+++ brigade.js
@@ -1,5 +1,17 @@
-const { events } = require("brigadier");
+const { events, Job , Group} = require("brigadier");
+const dest = "$GOPATH/src/github.com/technosophos/ulid";
 
-events.on("push", function(e, project) {
-  console.log("received push for commit ---------" + e.commit)
-})
+events.on("push", (e, p) => {
+  console.log(e.payload)
+  var gh = JSON.parse(e.payload)
+  var test = new Job("test", "golang:1.9")
+  test.tasks = [
+    "mkdir -p " + dest,
+    "cp -a /src/* " + dest,
+    "cd " + dest,
+    "go get -u github.com/golang/dep/cmd/dep",
+    "dep ensure",
+    "make test"
+  ];
+  test.run()
+});
